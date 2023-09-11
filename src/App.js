cc049c47d08c0c30f8ce619fdfbd73bb9e7d74e5
Bohdan Leliuk <bohdan.leliuk@gmail.com>
2023-09-11 11:02:28 +0300

    create first layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import "./App.scss";
 import SideBar from "./components/SideBar/SideBar";
 import { Title } from "./components/Title/Title";
 
 const App = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
   
+    function handleClick() {
+      setIsOpen(prev => !prev);
+    }
+
     return (
     <div className="app">
         <SideBar/>
+        <div className="side-left"/>
+        <div className="container-flex">
+          <div className="block">1</div>
+          <div className={ isOpen ? 'block' : 'block big-block'} onClick={handleClick}>2</div>
+          <div className="block">3</div>
+          <div className="block">4</div>
+          <div className="block">5</div>
+          <div className="block">6</div>
+          <div className="block">7</div>
+          <div className="block">8</div>
+          <div className="block">1</div>
+          <div className="block">2</div>
+          <div className="block">3</div>
+          <div className="block">4</div>
+          <div className="block">5</div>
+          <div className="block">6</div>
+          <div className="block">7</div>
+          <div className="block">8</div>
+          <div className="block">1</div>
+          <div className="block">2</div>
+          <div className="block">3</div>
+          <div className="block">4</div>
+          <div className="block">5</div>
+          <div className="block">6</div>
+          <div className="block">7</div>
+          <div className="block">8</div>
+        </div>
+        <div className="side-right"/>
     </div>
   );
 };
